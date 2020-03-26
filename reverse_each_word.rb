d7c44816b213896ad61e8c4192e1f59825609ec0
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 01:20:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence.split(",") #convert string to array
 new_arr = []
   sentence.each do |words| #iterate through elements
-    sentence.reverse
+    sentence.reverse()
   end
   
 end  
